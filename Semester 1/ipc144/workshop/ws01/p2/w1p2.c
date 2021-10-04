19c1f56fe1a47cfc4d1d3d9d27726ea7f1c67511
Wei-Chih Kao <82863012+me60714@users.noreply.github.com>
2021-10-04 02:06:00 -0400

    Add files via upload

---
+++ Semester 1/ipc144/workshop/ws01/p2/w1p2.c
@@ -0,0 +1,19 @@
+/*
+1.Full name:Wei-Chih Kao
+2.Student Number: 158000190
+3.Seneca Email Address:wkao3@myseneca.ca
+4.Section Information Code:ZAA
+*/
+
+#include <stdio.h>
+
+int main() {
+
+	printf("+-------------------------------+\n"); //not sure if these printf section can be simplify or not.
+	printf("| Introduction to C Programming |\n");
+	printf("+-------------------------------+\n");
+	printf("Program written by:\n");
+	printf("Wei-Chih Kao\n");
+
+	return 0;
+}
